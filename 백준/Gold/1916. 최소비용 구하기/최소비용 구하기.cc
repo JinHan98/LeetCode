729d7e2745e5fc5f04ee33e0d846a86f8664cf05
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2023-05-12 17:17:35 +0900

    [Gold V] Title: 최소비용 구하기, Time: 36 ms, Memory: 8660 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <cmath>
+using namespace std;
+void BFS(int a);
+vector<pair<int,int> > weight[1001];
+int graph[1001][1001];
+int val[1001];
+int N,M;
+int s,e,w;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    cin>>N>>M;
+    for(int i=1;i<=N;i++){
+        val[i]=1500000000;
+    }
+    for(int i=0;i<M;i++){
+        cin>>s>>e>>w;
+        weight[s].push_back({w,e});
+    }
+    cin>>s>>e;
+    BFS(s);
+    cout<<val[e];
+}
+void BFS(int a){
+    val[a]=0;
+    priority_queue<pair<int,int>,vector<pair<int,int> >,greater<pair<int,int> > >k;
+    k.push({0,a});
+    while(!k.empty()){
+        int current=k.top().second;
+        int distance=k.top().first;
+        k.pop();
+        if(val[current]<distance)
+            continue;
+        for(int i=0;i<weight[current].size();i++){
+            int next=weight[current][i].second;
+            int next_distance=weight[current][i].first+distance;
+            if(val[next]>next_distance){
+                val[next]=next_distance;
+                k.push({next_distance,next});
+            }
+        }
+    }
+}
