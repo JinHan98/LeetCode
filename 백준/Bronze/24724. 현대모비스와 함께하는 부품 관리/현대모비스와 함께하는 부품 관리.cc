c57a23814e357ae7642c8b4295cc21a36d8c2b1c
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2023-06-05 12:15:47 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int N;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    cin>>N;
+    for(int i=0;i<N;i++){
+        cout<<"Material Management "<<i+1<<'\n';
+        cout<<"Classification ---- End!"<<'\n';
+    }
+}
