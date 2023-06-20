c194cd7e15f6b09546c1ff3c1da14ef98b2968e1
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2023-06-20 23:57:48 +0900

    [Bronze I] Title: 배고파(Easy), Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28239. 배고파（Easy）/배고파（Easy）.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <cmath>
+using namespace std;
+
+string binary(long long a);
+int n;
+long long m;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>m;
+        if(m==1||m==2){
+            cout<<0<<' '<<0<<'\n';
+            continue;
+        }
+        string a=binary(m);
+        int y=a.size()-1;
+        int x=0;
+        for(int i=1;i<y;i++){
+            if(a[i]=='1'){
+                x=y-i;
+                break;
+            }
+        }
+        if(x==0&&m%2==0){
+            cout<<y-1<<' '<<y-1<<'\n';
+            continue;
+        }
+        cout<<x<<' '<<y<<'\n';
+    }
+    
+}
+string binary(long long a){
+    string ans;
+    if(a==0)
+        return "0";
+    while(a>0){
+        ans.push_back(a%2+'0');
+        a/=2;
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
