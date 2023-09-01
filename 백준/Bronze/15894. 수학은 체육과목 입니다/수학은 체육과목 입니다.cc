847514d3072ad7cdc04aee102f9894498f06584c
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2023-09-01 14:06:21 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <unordered_set>
+#include <algorithm>
+#include <queue>
+using namespace std;
+long long N;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    cin>>N;
+    cout<<4*N;
+}
