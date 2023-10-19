2a6c0a43553cd1441e9a771ba6d09d0b82b33f9f
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2023-10-19 18:49:41 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <limits.h>
+#include <unordered_map>
+#include <cmath>
+using namespace std;
+int T,C;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    cin>>T;
+    cout<<T/11*10;
+}
