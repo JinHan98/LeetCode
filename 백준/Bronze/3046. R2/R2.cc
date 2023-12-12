7fa8f9e2617e0d08136f298ccc9a14501565ff0e
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2023-12-12 23:16:48 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <unordered_set>
+#include <set>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int A,B;
+    cin>>A>>B;
+    cout<<2*B-A;
+}
