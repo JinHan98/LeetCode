179f04f8d213cabe47f3f6a09804a70337dd6bf9
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2023-12-31 23:43:43 +0900

    [Bronze V] Title: HI-ARC=?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+using namespace std;
+
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int A,B,C,D,E;
+    cin>>A>>B>>C>>D>>E;
+    cout<<(A*B)-(C*D*E);
+}
