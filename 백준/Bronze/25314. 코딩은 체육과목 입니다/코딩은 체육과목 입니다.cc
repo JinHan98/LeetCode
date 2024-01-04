8f8ee20f2424a013b98c045aa5653c186e453049
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-01-04 16:18:09 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+using namespace std;
+
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int N;
+    cin>>N;
+    for(int i=0;i<N/4;i++){
+        cout<<"long ";
+    }
+    cout<<"int";
+}
