5f47bbbaec47828e7e5e4d61c52dd566069d4ba3
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-01-14 14:27:56 +0900

    [Silver V] Title: 또 수열 문제야, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/31229. 또 수열 문제야/또 수열 문제야.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <set>
+using namespace std;
+int N;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    cin>>N;
+    for(int i=0;i<N;i++){
+        cout<<i*2+1<<' ';
+    }   
+}
