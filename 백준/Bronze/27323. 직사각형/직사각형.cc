3b40d20d7774ee9478aa437bf833de2e00795f42
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-01-16 18:33:20 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+using namespace std;
+int A,B;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    cin>>A>>B;
+    cout<<A*B;
+}
