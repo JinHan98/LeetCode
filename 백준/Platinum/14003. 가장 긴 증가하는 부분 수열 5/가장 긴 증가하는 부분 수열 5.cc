826d242cdf82829c808267e0155d068a6fe4c2a7
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-01-28 22:46:07 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 208 ms, Memory: 18216 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <stack>
+using namespace std;
+
+int N;
+vector<int> temp;
+int DP[1000000];
+int num[1000000];
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    cin>>N;
+    for(int i=0;i<N;i++){
+        cin>>num[i];
+    }
+    DP[0]=1;
+    int mx=0;
+    int mx_idx=0;
+    temp.push_back(num[0]);
+    for(int i=1;i<N;i++){
+        if(num[i]>temp.back()){
+            temp.push_back(num[i]);
+            DP[i]=temp.size();
+            if(DP[i]>mx){
+                mx=DP[i];
+                mx_idx=i;
+            }
+        }
+        else{
+            int idx=lower_bound(temp.begin(),temp.end(),num[i])-temp.begin();
+            DP[i]=idx+1;
+            temp[idx]=num[i];
+        }
+    }
+    stack<int> ans;
+    ans.push(num[mx_idx]);
+    int this_num=DP[mx_idx];
+    for(int i=mx_idx-1;i>=0;i--){
+        if(DP[i]==this_num-1){
+            ans.push(num[i]);
+            this_num--;
+            if(this_num==0)
+                break;
+        }
+    }
+    cout<<ans.size()<<'\n';
+    while(!ans.empty()){
+        cout<<ans.top()<<' ';
+        ans.pop();
+    }
+
+    return 0;
+    
+}
