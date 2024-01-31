f87fc356f30c420a311666c12bef67bd5babf43b
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-01-31 19:48:26 +0900

    [Gold V] Title: 멍멍이 쓰다듬기, Time: 0 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Gold/1669. 멍멍이 쓰다듬기/멍멍이 쓰다듬기.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <limits.h>
+using namespace std;
+vector<int> k;
+int T;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int a,b;
+    k.push_back(0);
+    int temp=1;
+    int idx=1;
+    bool up=false;
+    while(temp>0){
+        k.push_back(temp);
+        temp+=idx;
+        if(!up){
+            idx++;
+            up=true;
+        }
+        else{
+            up=false;
+        }
+    }
+    cin>>a>>b;
+    int ans=b-a;
+    cout<<lower_bound(k.begin(),k.end(),ans)-k.begin()<<'\n';
+}
