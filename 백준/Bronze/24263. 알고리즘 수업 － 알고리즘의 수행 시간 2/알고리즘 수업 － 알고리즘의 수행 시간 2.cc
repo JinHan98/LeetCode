26c4b6108c8ebc39ef8c0d8c18b13a94b804dc4c
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-02-01 15:35:31 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <stack>
+using namespace std;
+int A;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    cin>>A;
+    cout<<A<<'\n'<<1;
+    
+}
