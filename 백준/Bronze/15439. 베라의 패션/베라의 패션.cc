3ef26455f71fbfe43cf291d0a0151ab650cab3e1
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-02-01 15:37:14 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <stack>
+using namespace std;
+int N;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    cin>>N;
+    cout<<N*(N-1);
+    
+}
