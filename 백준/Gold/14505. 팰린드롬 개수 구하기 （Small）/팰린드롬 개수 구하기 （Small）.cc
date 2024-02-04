af35c77d23aa2bdc58f7fa071ccbb9128920a7d4
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-02-04 22:36:27 +0900

    [Gold III] Title: 팰린드롬 개수 구하기 (Small), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/14505. 팰린드롬 개수 구하기 （Small）/팰린드롬 개수 구하기 （Small）.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <stack>
+using namespace std;
+
+string S;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    cin>>S;
+    vector<int>DP(S.size());
+    for(int i=0;i<S.size();i++){
+        for(int j=i;j<S.size();j++){
+            if(S[i]==S[j]){
+                DP[j]++;
+                for(int k=j+1;k<S.size();k++){
+                    DP[j]+=DP[k];
+                }
+            }
+        }
+    }
+    int ans=0;
+    for(int i=0;i<S.size();i++){
+        ans+=DP[i];
+    }
+    cout<<ans;
+}
