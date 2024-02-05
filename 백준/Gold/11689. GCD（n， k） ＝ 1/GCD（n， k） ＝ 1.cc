ca5bcfd9edb8f8741116f11647944e7d273d7e62
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-02-05 22:29:55 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 36 ms, Memory: 6884 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <cmath>
+using namespace std;
+long long gcd(long long a,long long b);
+set<int> sosu;
+bool not_sosu[1000001];
+long long N;
+int main() {
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    long long ans=1;
+    cin>>N;
+    if(N==1){
+        cout<<1;
+        return 0;
+    }
+    not_sosu[0]=true;
+    not_sosu[1]=true;
+    for(int i=2;i<=1000000;i++){
+        if(!not_sosu[i]){
+            sosu.insert(i);
+            for(int j=i+i;j<=1000000;j+=i){
+                not_sosu[j]=true;
+            }
+        }
+    }
+    for(auto iter=sosu.begin();iter!=sosu.end();iter++){
+        if(N==1)
+            break;
+        else{
+            int num=0;
+            long long a=gcd(N,*iter);
+            while(a==*iter){
+                num++;
+                N/=(*iter);
+                a=gcd(N,*iter);
+            }
+            if(num!=0){
+                if(num==1){
+                    ans*=(*iter-1);
+                }
+                else{
+                    ans*=(pow(*iter,num-1)*(*iter-1));
+                }
+            }
+        }
+    }
+    if(N!=1){
+        ans*=(N-1);
+        cout<<ans;
+    }
+    else
+        cout<<ans;
+    return 0;
+}
+long long gcd(long long a,long long b){
+    if(b==0)
+        return a;
+    else
+        return gcd(b,a%b);
+}
