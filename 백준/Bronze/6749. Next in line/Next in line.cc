f22fda8d2b24865a36f0ccfa791189f692a7c456
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-04-05 13:27:48 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+using namespace std;
+
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int A,B;
+    cin>>A>>B;
+    cout<<B+(B-A);
+}
