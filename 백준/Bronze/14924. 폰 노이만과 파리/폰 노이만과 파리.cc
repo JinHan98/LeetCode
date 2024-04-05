66c59582f2c230303189cf3be15b4408aba37574
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-04-05 14:12:55 +0900

    [Bronze IV] Title: 폰 노이만과 파리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14924. 폰 노이만과 파리/폰 노이만과 파리.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <unordered_set>
+#include <set>
+using namespace std;
+
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int A,B,C;
+    cin>>A>>B>>C;
+    cout<<B*(C/(2*A));
+    
+}
