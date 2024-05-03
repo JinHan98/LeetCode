6767f435447ef66d544b8bde3ca88bf29e968ec5
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 12:01:40 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int N;
+    cin>>N;
+    for(int i=0;i<N;i++){
+        cout<<"SciComLove\n";
+    }
+
+}
