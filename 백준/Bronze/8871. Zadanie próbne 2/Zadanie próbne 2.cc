b125b13fe961128dbcd20908ee1a83832ddb49be
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 11:21:27 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int N;
+    cin>>N;
+    cout<<(N+1)*2<<' '<<(N+1)*3;
+
+}
