05d13074af190690f8868b20f9df0cd93c05e77f
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 11:29:58 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int N,K,a,b;
+    cin>>N>>K;
+    for(int i=0;i<N;i++){
+        cin>>a>>b;
+    }
+    cout<<"비와이";
+    
+
+}
