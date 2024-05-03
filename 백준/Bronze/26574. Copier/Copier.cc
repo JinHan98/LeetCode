e286e0820457db3a77a03ee29853f726c469875b
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 13:03:42 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int N;
+    int ans=0;
+    cin>>N;
+    for(int i=0;i<N;i++){
+        int temp;
+        cin>>temp;
+        cout<<temp<<' '<<temp<<'\n';
+    }
+}
