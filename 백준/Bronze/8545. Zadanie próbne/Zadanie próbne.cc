5c839724d80da7bb3df52242ac2381cc9a777ae3
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 11:19:11 +0900

    [Bronze V] Title: Zadanie próbne, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    string a;
+    cin>>a;
+    reverse(a.begin(),a.end());
+    cout<<a;
+
+}
