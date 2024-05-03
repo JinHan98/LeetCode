59fb5d226cae018a9ed24ed34167aece96c6be03
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 11:36:21 +0900

    [Bronze V] Title: 나는 누구인가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    cout<<"I'm Sexy";
+
+}
