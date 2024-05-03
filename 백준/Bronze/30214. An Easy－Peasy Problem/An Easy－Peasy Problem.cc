51cae0995c14ff999e078706ddfbdc15e01776ef
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 16:57:37 +0900

    [Bronze V] Title: An Easy-Peasy Problem, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30214. An Easy－Peasy Problem/An Easy－Peasy Problem.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+#include <map>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int A,B;
+    cin>>A>>B;
+    if(A*2>=B){
+        cout<<"E";
+    }
+    else{
+        cout<<"H";
+    }
+
+}
