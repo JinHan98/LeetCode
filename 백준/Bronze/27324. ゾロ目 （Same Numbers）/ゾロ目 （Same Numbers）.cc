f28ed770f3860e2312f77cea2e0500a583411056
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 13:11:08 +0900

    [Bronze V] Title: ゾロ目 (Same Numbers), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27324. ゾロ目 （Same Numbers）/ゾロ目 （Same Numbers）.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    string s;
+    cin>>s;
+    if(s[0]==s[1]){
+        cout<<1;
+    }
+    else{
+        cout<<0;
+    }
+}
