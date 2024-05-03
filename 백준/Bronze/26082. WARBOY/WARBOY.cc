08daf8c55273ab30ceee9097d7529f87296c1e60
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 12:56:17 +0900

    [Bronze V] Title: WARBOY, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<b/a*c*3;
+}
