65862b9c77f0312f2b13a5b15dec93be0af2d9f7
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 11:22:45 +0900

    [Bronze V] Title: NFC West vs North, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10170. NFC West vs North/NFC West vs North.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "NFC West       W   L  T" << endl
+    << "-----------------------" << endl
+    << "Seattle        13  3  0" << endl
+    << "San Francisco  12  4  0" << endl
+    << "Arizona        10  6  0" << endl
+    << "St. Louis      7   9  0" << endl << endl
+    << "NFC North      W   L  T" << endl
+    << "-----------------------" << endl
+    << "Green Bay      8   7  1" << endl
+    << "Chicago        8   8  0" << endl
+    << "Detroit        7   9  0" << endl
+    << "Minnesota      5  10  1";
+    return 0;
+}
