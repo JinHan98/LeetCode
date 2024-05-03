c7abfc1a749fdbe3802124abe3abb93019b183c0
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 13:05:40 +0900

    [Bronze V] Title: Serca, Time: 100 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N;
+	while (N--) {
+		cout << " @@@   @@@ " << '\n';
+		cout << "@   @ @   @" << '\n';
+		cout << "@    @    @" << '\n';
+		cout << "@         @" << '\n';
+		cout << " @       @ " << '\n';
+		cout << "  @     @  " << '\n';
+		cout << "   @   @   " << '\n';
+		cout << "    @ @    " << '\n';
+		cout << "     @     " << '\n';
+	}
+}
