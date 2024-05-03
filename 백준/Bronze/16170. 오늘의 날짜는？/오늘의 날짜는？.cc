ae7a6ef301047ccc238ca007a2c80b2ab919ffbb
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 11:39:59 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    cout<<2024<<'\n';
+    cout<<"05"<<'\n';
+    cout<<"03"<<'\n';
+
+}
