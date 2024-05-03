f19f466c5c5f596f5cafc05fe89b0d7ade89fad1
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 11:38:51 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    long long a,b;
+    cin>>a>>b;
+    cout<<(a==b);
+
+}
