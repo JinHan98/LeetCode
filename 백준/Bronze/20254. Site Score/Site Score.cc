da755baa4e418418e59e2f271b4d5e4a2edbab09
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 11:56:05 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int Ur,Tr,Uo,To;
+    cin>>Ur>>Tr>>Uo>>To;
+    cout<<56*Ur+24*Tr+14*Uo+6*To;
+
+}
