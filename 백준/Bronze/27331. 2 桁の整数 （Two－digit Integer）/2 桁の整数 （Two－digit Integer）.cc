8528bd71fc8e1eb01fc03d27cc84b34e37576d55
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 13:13:09 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    char a,b;
+    cin>>a>>b;
+    cout<<a<<b;
+}
