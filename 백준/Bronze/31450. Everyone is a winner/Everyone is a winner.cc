16c73a95b3bbac39cfb1dad1d1b6b73ea09f567a
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 17:14:17 +0900

    [Bronze V] Title: Everyone is a winner, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31450. Everyone is a winner/Everyone is a winner.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+#include <map>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int A,B;
+    cin>>A>>B;
+    if(A%B==0){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+}
