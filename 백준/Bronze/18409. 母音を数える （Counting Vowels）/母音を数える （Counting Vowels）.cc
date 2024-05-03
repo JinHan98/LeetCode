cd55ccd8607463dfcbed9120ddea5cab6b197fdb
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 11:54:04 +0900

    [Bronze V] Title: 母音を数える (Counting Vowels), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/18409. 母音を数える （Counting Vowels）/母音を数える （Counting Vowels）.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int N;
+    cin>>N;
+    string s;
+    int ans=0;
+    cin>>s;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+            ans++;
+    }
+    cout<<ans;
+
+}
