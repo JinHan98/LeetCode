25b0f2a55ed3dda03997ad3cb8a9be64007036eb
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 12:33:56 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    char a;
+    cin>>a;
+    if(a=='n'||a=='N'){
+        cout<<"Naver D2";
+    }
+    else{
+        cout<<"Naver Whale";
+    }
+}
