c81b04eb6a5eced293ed6fa2b6c543bd3eddf603
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 13:02:52 +0900

    [Bronze V] Title: Mathematics, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int N;
+    int ans=0;
+    cin>>N;
+    for(int i=0;i<N;i++){
+        int temp;
+        cin>>temp;
+        ans+=temp;
+    }
+    cout<<ans;
+}
