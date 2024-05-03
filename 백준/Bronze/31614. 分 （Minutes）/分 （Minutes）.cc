c6cdffcd011b2c6101166ba47d229a792c96cb73
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 17:19:09 +0900

    [Bronze V] Title: 分 (Minutes), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31614. 分 （Minutes）/分 （Minutes）.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+#include <map>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int A,B;
+    cin>>A>>B;
+    cout<<A*60+B;
+}
