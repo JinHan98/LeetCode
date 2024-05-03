637b21194e82462353278e8ee6a36ec7abd5770b
JinHan98 <55446106+JinHan98@users.noreply.github.com>
2024-05-03 17:03:18 +0900

    [Bronze V] Title: Java Warriors, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+#include <queue>
+#include <cmath>
+#include <map>
+using namespace std;
+int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int N;
+    cin>>N;
+    cout<<4000*N;
+}
